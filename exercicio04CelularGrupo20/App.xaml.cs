ad2f0a6592b66e9ec746cd0299dfd9f2c374e8f7
Cleslley <cleslleymouralima2015@gmail.com>
2025-04-15 11:00:00 -0300

    feat: Cell App

---
+++ exercicio04CelularGrupo20/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using exercicio04CelularGrupo20.Views;
+
+namespace exercicio04CelularGrupo20
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Principal();
+        }
+    }
+}
